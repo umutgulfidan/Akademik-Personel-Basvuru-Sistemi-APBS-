39baa0cadc5424c162a47f61bc6526acae1e4a1c
umutgulfidan <umutgulfidan@gmail.com>
2025-02-27 17:17:21 +0300

    Add project files.

---
+++ DataAccess/Concretes/Context.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concretes;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=APBS_Database;Trusted_Connection=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppUser>().HasIndex(x => x.Email).IsUnique();
+            builder.Entity<AppUser>().HasIndex(x => x.NationalityId).IsUnique();
+            builder.Entity<AppUser>().HasIndex(x => x.PhoneNumber).IsUnique();
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
