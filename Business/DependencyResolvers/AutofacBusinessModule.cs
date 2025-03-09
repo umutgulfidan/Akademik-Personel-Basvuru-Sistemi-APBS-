6dc0e8e359c753b9095583ae24181afc4c67fdd2
umutgulfidan <umutgulfidan@gmail.com>
2025-03-09 15:43:49 +0300

    Code refactor for AutofacBusinessModule.

--- Business/DependencyResolvers/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/AutofacBusinessModule.cs
@@ -6,55 +6,65 @@ using Business.Abstracts;
 using Business.Concrete;
 using Business.Concretes;
 using Business.Mapping;
-using Core.Aspects.Autofac.Logger;
 using Core.Utilities.Interceptors;
 using Core.Utilities.Security.Jwt;
 using DataAccess.Abstracts;
 using DataAccess.Concretes.EntitiyFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Business.DependencyResolvers
 {
-    public class AutofacBusinessModule : Module
+    public class AutofacBusinessModule : Autofac.Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+            // Servis ve Dal sınıflarının tek bir metodda kaydı
+            RegisterServiceAndDal(builder);
 
+            // AutoMapper Konfigürasyonu
+            RegisterAutoMapper(builder);
+
+            // Interceptors ve Aspectler için yapılandırma
+            RegisterInterceptors(builder);
+        }
+
+        private void RegisterServiceAndDal(ContainerBuilder builder)
+        {
+            // Servis ve DataAccess katmanındaki sınıfları toplu şekilde kaydetmek için
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
             builder.RegisterType<AlanManager>().As<IAlanService>().SingleInstance();
             builder.RegisterType<EfAlanDal>().As<IAlanDal>().SingleInstance();
             builder.RegisterType<BolumManager>().As<IBolumService>().SingleInstance();
             builder.RegisterType<EfBolumDal>().As<IBolumDal>().SingleInstance();
-
-
             builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
-            builder.RegisterType<LoggerAspect>();  // LoggerAspect'i burada kaydediyoruz
+        }
 
-
-            // AutoMapper Konfigürasyonu
+        private void RegisterAutoMapper(ContainerBuilder builder)
+        {
+            // AutoMapper konfigürasyonunu buradan yapıyoruz
             builder.Register(c =>
             {
                 var config = new MapperConfiguration(cfg =>
                 {
-                    // Mapping profilini buradan ekleyebilirsiniz
-                    cfg.AddProfile(new MappingProfile()); // MappingProfile'ı projede oluşturduğunuz yerden çağırıyorsunuz.
+                    cfg.AddProfile(new MappingProfile()); // MappingProfile'ı burada belirtiyoruz
                 });
                 return config.CreateMapper(); // AutoMapper'ı döndürüyoruz.
             }).As<IMapper>().SingleInstance();
+        }
 
-
-            // Interceptors ve Aspectler için yapılandırma
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces().EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions()
-            {
-                Selector = new AspectInterceptorSelector()
-            }).SingleInstance();
+        private void RegisterInterceptors(ContainerBuilder builder)
+        {
+            // AspectInterceptorSelector'ı ve bütün interceptorları kaydediyoruz
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                   .AsImplementedInterfaces()
+                   .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions
+                   {
+                       Selector = new AspectInterceptorSelector()
+                   })
+                   .SingleInstance();
         }
     }
 }
