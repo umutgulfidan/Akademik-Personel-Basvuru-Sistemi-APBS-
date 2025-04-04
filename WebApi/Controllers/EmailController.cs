bce28e18153ed5c5e41801c7d327adddf973d823
umutgulfidan <umutgulfidan@gmail.com>
2025-04-04 17:16:54 +0300

    Added HasEmailPermission method for check to send email endpoints.

--- WebApi/Controllers/EmailController.cs
+++ WebApi/Controllers/EmailController.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Abstracts;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Entities.Dtos.Email; // Bu dosyayı oluşturmanız gerekecek
+using Entities.Dtos.Email;
+using Business.Constants;
+using Core.Extensions.Claims; // Bu dosyayı oluşturmanız gerekecek
 
 namespace WebApi.Controllers
 {
@@ -16,9 +18,23 @@ namespace WebApi.Controllers
             _emailService = emailService;
         }
 
+        private bool HasEmailPermission()
+        {
+            if (User?.Identity?.IsAuthenticated != true)
+                return false;
+
+            var roles = User.ClaimRoles();
+            return roles.Contains("Admin") || roles.Contains("Yonetici");
+        }
+
         [HttpPost("send")]
         public async Task<IActionResult> SendEmail(SendEmailDto sendEmailDto)
         {
+            if (!HasEmailPermission())
+            {
+                return Unauthorized();
+            }
+
             var result = await _emailService.SendEmail(sendEmailDto.Email, sendEmailDto.Subject, sendEmailDto.Body);
             if (result.IsSuccess)
             {
@@ -30,6 +46,11 @@ namespace WebApi.Controllers
         [HttpPost("send-to-user")]
         public async Task<IActionResult> SendEmailToUser(SendUserEmailDto sendUserEmailDto)
         {
+            if (!HasEmailPermission())
+            {
+                return Unauthorized();
+            }
+
             var result = await _emailService.SendEmailToUser(sendUserEmailDto.UserId, sendUserEmailDto.Subject, sendUserEmailDto.Body);
             if (result.IsSuccess)
             {
