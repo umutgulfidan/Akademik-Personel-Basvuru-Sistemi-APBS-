694c707bb4c5f49b69cc5670f207a5dc240c8310
umutgulfidan <umutgulfidan@gmail.com>
2025-04-05 12:29:35 +0300

    feat: add DashboardManager, DashboardController.

---
+++ Business/Concretes/DashboardManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstracts;
+using DataAccess.Concretes;
+using Entities.Dtos.Dashboard;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class DashboardManager : IDashboardService
+    {
+        private readonly Context _context;
+
+        public DashboardManager(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<DashboardStats> GetDashboardStatsAsync()
+        {
+            var totalUsers = await _context.Users.CountAsync();
+            var activeUsers = await _context.Users.CountAsync(u => u.Status);
+            var passiveUsers = await _context.Users.CountAsync(u => !u.Status);
+            var totalIlan = await _context.Ilanlar.CountAsync();
+            var activeIlan = await _context.Ilanlar.CountAsync(i => i.Status);
+            var passiveIlan = await _context.Ilanlar.CountAsync(i => !i.Status);
+            var totalAlan = await _context.Alanlar.CountAsync();
+            var totalBolum = await _context.Bolumler.CountAsync();
+
+            return new DashboardStats
+            {
+                TotalUsers = totalUsers,
+                ActiveUsers = activeUsers,
+                PassiveUsers = passiveUsers,
+                TotalIlan = totalIlan,
+                ActiveIlan = activeIlan,
+                PassiveIlan = passiveIlan,
+                TotalAlan = totalAlan,
+                TotalBolum = totalBolum
+            };
+        }
+    }
+}
---
+++ WebApi/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        // DashboardStatsService'i constructor üzerinden alıyoruz
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        // GET api/dashboard/stats
+        [HttpGet("stats")]
+        public async Task<IActionResult> GetDashboardStats()
+        {
+            // İstatistikleri alıyoruz
+            var dashboardStats = await _dashboardService.GetDashboardStatsAsync();
+
+            // İstatistikler başarıyla alındıysa, veriyi JSON formatında döndürüyoruz
+            return Ok(dashboardStats);
+        }
+    }
+}
